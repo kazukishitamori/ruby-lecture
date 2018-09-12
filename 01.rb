dc91a50845adfd24d45f0d132de98654ffaccf64
shitamori kazuki <nyan299-kazu@ezweb.ne.jp>
2018-09-12 08:15:40 +0000

    add 01.rb and modified hello.rb

---
+++ 01.rb
@@ -0,0 +1,8 @@
+def greeting(country)
+ if country == 'japan'
+  puts 'こんいちは'
+ else
+  puts 'hello'
+ end
+end
+greeting('japan')
--- hello.rb
+++ hello.rb
@@ -1,1 +1,3 @@
- puts "Hellow Ruby"
+ puts 1.to_s#文の途中でも入れられます。
+ puts nil.to_s
+ #ここはコメント文です()
